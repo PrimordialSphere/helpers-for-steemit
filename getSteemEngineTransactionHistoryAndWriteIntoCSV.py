4b5b6472f1c1e9b52326283f6552505e297f4dd9
unknown <Oliver@NAMENA.AC.AIXVOX.NET>
2020-01-29 13:46:03 +0100

    works stable now

--- getSteemEngineTransactionHistoryAndWriteIntoCSV.py
+++ getSteemEngineTransactionHistoryAndWriteIntoCSV.py
@@ -5,12 +5,12 @@ from datetime import date
 conv = lambda i : i or ''
 s = SteemEngineToken()
 today = date.today()
-csv_columns = ['Timestamp', 'Symbol', 'Quantity', 'Memo', 'Sender', 'To']
+csv_columns = ['Timestamp', 'Symbol', 'Quantity', 'Memo', 'Sender', 'To', 'Raw Data', 'Block']
 
 with open('%s_beerlover_overview.csv' % today, 'w', newline='') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
     writer.writeheader()
-    for tx in s.list_transactions('beerlover', 'BEER', 9000):
+    for tx in s.list_transactions('someguy', 'SOMETOKEN', 1000):
         writer.writerow({
             'Timestamp' : conv(str(tx['timestamp'])), 
             'Symbol' : conv(tx['symbol']), 
@@ -18,11 +18,11 @@ with open('%s_beerlover_overview.csv' % today, 'w', newline='') as csvfile:
             'Memo' : conv(tx['memo']), 
             'Sender' : conv(tx['sender']), 
             'To' : conv(tx['to']),
-            ##str(tx['raw_data']),
-            ##str(tx['block'])
+            'Raw Data' : str(tx['raw_data']),
+            'Block' : str(tx['block'])
                         })
         print(
-            tx
+            ##tx
             ##tx['timestamp'], 
             ##tx['symbol'], 
             ##tx['quantity'], 
