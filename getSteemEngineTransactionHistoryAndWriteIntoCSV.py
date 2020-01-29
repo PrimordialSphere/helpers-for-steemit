e52615b01fd1fc7b9ec7416a79ca38062124ef40
unknown <Oliver@NAMENA.AC.AIXVOX.NET>
2020-01-29 13:41:57 +0100

    retrieving data and writing into csv works

--- getSteemEngineTransactionHistoryAndWriteIntoCSV.py
+++ getSteemEngineTransactionHistoryAndWriteIntoCSV.py
@@ -1,4 +1,34 @@
-from steemengine.api import Api
-api = Api()
+import csv
+from privex.steemengine import SteemEngineToken
+from datetime import date
 
-print(api.get_history("beerlover", "BEER"))
+conv = lambda i : i or ''
+s = SteemEngineToken()
+today = date.today()
+csv_columns = ['Timestamp', 'Symbol']
+
+with open('%s_beerlover_overview.csv' % today, 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+    writer.writeheader()
+    for tx in s.list_transactions('beerlover', 'BEER', 9000):
+        writer.writerow({
+            'Timestamp' : conv(str(tx['timestamp'])), 
+            'Symbol' : conv(tx['symbol']), 
+            'Quantity' : conv(tx['quantity']), 
+            'Memo' : conv(tx['memo']), 
+            'Sender' : conv(tx['sender']), 
+            'To' : conv(tx['to']),
+            ##str(tx['raw_data']),
+            ##str(tx['block'])
+                        })
+        print(
+            tx
+            ##tx['timestamp'], 
+            ##tx['symbol'], 
+            ##tx['quantity'], 
+            ##tx['memo'],
+            ##tx['sender'], 
+            ##tx['to'],
+            ##str(tx['raw_data']),
+            ##str(tx['block'])
+        )
