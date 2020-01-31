5df01f4061e349c4e28a00ad6e5cf59b644d1e79
unknown <Oliver@NAMENA.AC.AIXVOX.NET>
2020-01-31 09:36:00 +0100

    export to xlsx via pandas added

--- getSteemEngineTransactionHistoryAndWriteIntoCSV.py
+++ getSteemEngineTransactionHistoryAndWriteIntoCSV.py
@@ -1,4 +1,5 @@
 import csv
+import pandas as pd
 from privex.steemengine import SteemEngineToken
 from datetime import date
 
@@ -10,25 +11,30 @@ csv_columns = ['Timestamp', 'Symbol', 'Quantity', 'Memo', 'Sender', 'To', 'Raw D
 with open('%s_beerlover_overview.csv' % today, 'w', newline='') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
     writer.writeheader()
-    for tx in s.list_transactions('someguy', 'SOMETOKEN', 1000):
-        writer.writerow({
-            'Timestamp' : conv(str(tx['timestamp'])), 
-            'Symbol' : conv(tx['symbol']), 
-            'Quantity' : conv(tx['quantity']), 
-            'Memo' : conv(tx['memo']), 
-            'Sender' : conv(tx['sender']), 
-            'To' : conv(tx['to']),
-            'Raw Data' : str(tx['raw_data']),
-            'Block' : str(tx['block'])
+    for x in range(0,5):
+        for tx in s.list_transactions('someguy', 'TOKEN', 500, x*500):
+            writer.writerow({
+                'Timestamp' : conv(str(tx['timestamp'])), 
+                'Symbol' : conv(tx['symbol']), 
+                'Quantity' : str(tx['quantity']), 
+                'Memo' : conv(tx['memo']), 
+                'Sender' : conv(tx['sender']), 
+                'To' : conv(tx['to']),
+                'Raw Data' : str(tx['raw_data']),
+                'Block' : str(tx['block'])
                         })
-        print(
-            ##tx
-            ##tx['timestamp'], 
-            ##tx['symbol'], 
-            ##tx['quantity'], 
-            ##tx['memo'],
-            ##tx['sender'], 
-            ##tx['to'],
-            ##str(tx['raw_data']),
-            ##str(tx['block'])
+            print(
+                ##tx
+                ##tx['timestamp'], 
+                ##tx['symbol'], 
+                ##str(tx['quantity']), 
+                ##type(tx['quantity'])
+                ##tx['memo'],
+                ##tx['sender'], 
+                ##tx['to'],
+                ##str(tx['raw_data']),
+                ##str(tx['block'])
         )
+            
+data = pd.read_csv('%s_beerlover_overview.csv' % today)
+data.to_excel('%s_beerlover_overview.xlsx' % today)
